9509c5ec7a2f611d40718f254b6ea64ddf250cd6
npetrovski <to.petrovski@gmail.com>
2016-02-06 20:13:24 +0200

    jphar v2.0.0

--- src/test/java/name/npetrovski/jphar/BasicTest.java
+++ src/test/java/name/npetrovski/jphar/BasicTest.java
@@ -2,15 +2,17 @@ package name.npetrovski.jphar;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.apache.commons.compress.utils.IOUtils;
 
 public class BasicTest extends TestCase {
 
-    private static final String testPharFile = "test-" + String.valueOf((System.currentTimeMillis() / 1000L)) + ".phar";
+    private static final String testPharFile = "src/test/resources/test-" + String.valueOf((System.currentTimeMillis() / 1000L)) + ".phar";
 
     /**
      * Returns the test suite.
@@ -26,7 +28,7 @@ public class BasicTest extends TestCase {
         super.setUp();
     }
 
-    public void testCreated() throws IOException, NoSuchAlgorithmException {
+    public void testCreateNew() throws IOException, NoSuchAlgorithmException {
 
         // Create
         final Phar p = new Phar(testPharFile);
@@ -47,12 +49,26 @@ public class BasicTest extends TestCase {
 
     }
 
-    public void testParse() throws IOException {
+    public void testPhpunit() throws IOException {
+        final Phar p = new Phar("src/test/resources/phpunit-5.2.1.phar");
+        assertEquals(542, p.getEntries().size());
+        assertEquals(Compression.Type.NONE, p.getManifest().getCompression().getType());
+        assertEquals("1.1.0", p.getManifest().getVersion().getVersion());
+    }
 
-        // READ
-        final Phar pa = new Phar(testPharFile);
-
-        assertEquals(Compression.Type.ZLIB, pa.getManifest().getCompression().getType());
+    public void testCodeception() throws IOException {
+        final Phar p = new Phar("src/test/resources/codecept.phar");
+        assertEquals(965, p.getEntries().size());
+        assertEquals(Compression.Type.ZLIB, p.getManifest().getCompression().getType());
+        assertEquals("1.1.0", p.getManifest().getVersion().getVersion());
+        
+        Entry entry = p.findEntry("src/Codeception/Events.php");
+   
+        assertNotNull(entry);
+        
+        String data = new String(IOUtils.toByteArray(entry.getInputStream()));
+        
+        assertTrue(data.contains("SUITE_INIT"));
     }
 
     @Override
